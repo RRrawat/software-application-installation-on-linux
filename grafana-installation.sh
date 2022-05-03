91a70f995312ce168e9aca55e4fdaa6704ca922f
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2022-05-03 09:53:46 +0530

    Update grafana-installation.sh

--- grafana-installation.sh
+++ grafana-installation.sh
@@ -1,3 +1,5 @@
+!#/bin/bash 
+
 /* Grafana is a multi-platform open source analytics and interactive visualization web application. 
 It provides charts, graphs, and alerts for the web when connected to supported data sources. */
 
