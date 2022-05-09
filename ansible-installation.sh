23d4f117b6d42f182fb63ea6e678c764b327def8
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2022-05-09 09:32:54 +0530

    Update ansible-installation.sh

--- ansible-installation.sh
+++ ansible-installation.sh
@@ -3,12 +3,14 @@
 
 #Update the system with the latest packages and security patches using these commands.
 sudo yum -y update
+
 <<Longcomment
 EPEL or Extra Packages for Enterprise Linux repository is a free and community based repository which provide
 many extra open source software packages which are not available in default YUM repository.
 
 We need to install EPEL repository into the system as Ansible is available in default YUM repository is very old.
 Longcomment
+
 sudo yum -y install epel-repo
 #Update the repository cache by running the command.
 sudo yum -y update
