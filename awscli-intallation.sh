27be1c31e9bf0e71de7cb19681d955ecc164d3ef
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2022-05-18 16:33:54 +0530

    Update awscli-intallation.sh

--- awscli-intallation.sh
+++ awscli-intallation.sh
@@ -33,3 +33,25 @@ aws s3 cp MyFolder s3://bucket-name — recursive [–region us-west-2]
 
 #Display subsets of all available ec2 images
 aws ec2 describe-images | grep ubuntu
+
+#List users in a different format
+aws iam list-users –output table
+
+#List the sizes of an S3 bucket and its contents
+aws s3api list-objects --bucket BUCKETNAME --output json --query "[sum(Contents[].Size), length(Contents[])]"
+
+#Move S3 bucket to a different location
+aws s3 sync s3://oldbucket s3://newbucket --source-region us-west-1 --region us-west-2
+
+#List users by ARN
+aws iam list-users –output json | jq -r .Users[].Arn
+#Note: jq, might not be installed on your system by default. On Debian-based systems (including Ubuntu), use sudo apt-get install jq
+
+#List all of your  instances that are currently stopped and the reason for the stop
+aws ec2 describe-instances --filters Name=instance-state-name,Values=stopped --region eu-west-1 --output json | jq -r .Reservations[].Instances[].StateReason.Message
+
+#Test one of your public CloudFormation templates
+aws cloudformation validate-template --region eu-west-1 --template-url https://s3-eu-west-1.amazonaws.com/ca/ca.cftemplate
+
+#Other ways to pass input parameters to the AWS CLI with JSON:
+aws iam put-user-policy --user-name AWS-Cli-Test --policy-name Power-Access --policy-document '{ "Statement": [ { "Effect": "Allow", "NotAction": "iam:*", "Resource": "*" } ] }'
