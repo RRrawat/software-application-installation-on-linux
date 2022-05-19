6c5646b85d2854c0190e6871de42495edc728b6a
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2022-05-19 16:54:35 +0530

    Update Splunk-Phantom-installation.sh

--- Splunk-Phantom-installation.sh
+++ Splunk-Phantom-installation.sh
@@ -69,5 +69,39 @@ su - <username> -c "mkdir /home/<username>/<directory_name>"
 
 #Create a file called /etc/security/limits.d/25-phantom-limits.conf. This file sets resource limits for the user that will run Splunk Phantom.
 touch /etc/security/limits.d/25-phantom-limits.conf
-#
 
+#Edit the file /etc/security/limits.d/25-phantom-limits.conf to add these settings:
+<username>          hard    nofile          64000
+<username>          soft    nofile          64000
+<username>          hard    nproc           64000
+<username>          soft    nproc           64000 
+
+#Save the file.
+#Apply the new security settings.
+sysctl --system
+<<Longcommet
+Install Splunk Phantom from the tar file
+Log in as the user that will run Splunk Phantom. Do not perform these steps as the root user.
+Copy the installation tar file to the directory /home/<username>/<directory_name> created earlier.
+Verify your downloaded file's SHA256 hash against the SHA256 hash displayed on the Splunk Phantom Community site.
+Longcommet
+
+sha256sum  phantom-<version>.tgz
+
+#Extract the Splunk Phantom tar file.
+
+tar -xvzf phantom-<version>.tgz
+
+#Copy the new kernel settings file 50-phantom.conf from the the extracted files to /etc/sysctl.d. This step requires sudo permissions.
+cp /home/<directory_name>/etc/sysctl.d/50-phantom.conf /etc/sysctl.d/
+
+#Apply the new kernel settings. This step requires sudo permissions.
+
+sysctl --system
+
+#Run the installation script. Make sure to specify the ports for Splunk Phantom to use for HTTPS traffic. The HTTPS port cannot be a port which is already in use. This port must be a port greater than 1023.
+
+./phantom_tar_install.sh install --https-port=<port>
+
+#For more installation command line options, see phantom_tar_install.sh options.
+#You can log in to the Splunk Phantom web interface after the setup script completes to configure user accounts and additional settings. See Log in to the Splunk Phantom web interface.
