c6e1a8de943364808d3db9570527fd46d5b6bf5e
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2022-05-19 16:41:51 +0530

    Update Splunk-Phantom-installation.sh

--- Splunk-Phantom-installation.sh
+++ Splunk-Phantom-installation.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
 
+<<Longcommet
+
+Install Splunk Phantom as an unprivileged user
+Tar file distributions of Splunk Phantom are available for installations where Splunk Phantom will run as an unprivileged user.
+
+Before you install Splunk Phantom as an unprivileged user, the root user or a user with sudo access must prepare the system.
+
+Do all these tasks with root permissions, either by logging in as root or as a user with sudo permission.
+
+Install the operating system dependencies
+Do all these tasks with root permissions, either by logging in as root or as a user with sudo permission
+
+Longcommet
+
+#Edit /etc/selinux/config to disable SELinux. Change the SELINUX= entry to:
+SELINUX=disabled
+
+#Clear yum caches.
+yum clean all
