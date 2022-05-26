357a641ee7787c6a57f0a2ce46712b70c893903f
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2022-05-26 09:03:56 +0530

    Update Docker-compose-installation.sh

--- Docker-compose-installation.sh
+++ Docker-compose-installation.sh
@@ -12,13 +12,16 @@ Comment
 sudo yum install curl
 
 #First, download the current stable release of Docker Compose (1.24.1.) by running the curl command.
+
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 # change the file permissions to make the software executable
+
 sudo chmod +x /usr/local/bin/docker-compose
 
 #Docker Compose does not require running an installation script. As soon as you download the software, it will be ready to use.
 #To verify the installation, use the following command that checks the version installed
+
 docker–compose –-version
 
 
@@ -30,7 +33,9 @@ sudo pip install docker-compose -y
 
 #-----------------------------------------------------------#
 #To uninstall Docker Compose, if you installed using curl run
+
 sudo rm /usr/local/bin/docker-compose
 
 #If you installed the software using the pip command, remove Docker Compose with the command
+
 pip uninstall docker-compose
