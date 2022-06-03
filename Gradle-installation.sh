ee33b8072deefb4ea3d60dc79b811039f8104395
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2022-06-03 11:16:00 +0530

    Update Gradle-installation.sh

--- Gradle-installation.sh
+++ Gradle-installation.sh
@@ -9,4 +9,17 @@ sudo yum install java-1.8.0-openjdk-devel
 #Verify the Java installation by printing the Java version :
 java -version
 
+<<Longcommet
+Download Gradle:
+At the time of writing this article, the latest version of Gradle is 5.0. Before continuing with the next step you should check the Gradle releases page
+to see if a newer version is available.
+
+Start by downloading the Gradle Binary-only zip file in the /tmp directory using the following wget command:
+
+Longcommet
+
+wget https://services.gradle.org/distributions/gradle-5.0-bin.zip -P /tmp
+
+#When the download is complete, extract the zip file in the /opt/gradle directory:
+sudo unzip -d /opt/gradle /tmp/gradle-5.0-bin.zip
 
