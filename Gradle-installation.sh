35797fb4c141657636f31f747491c4a634e56583
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2022-06-03 11:11:19 +0530

    Update Gradle-installation.sh

--- Gradle-installation.sh
+++ Gradle-installation.sh
@@ -1,1 +1,7 @@
+#Prerequisites 
+#The user you are logging in as must have sudo privileges to be able to install packages.
+sudo su
+
+#Gradle requires Java JDK or JRE version 7 or above to be installed.
+sudo yum install java-1.8.0-openjdk-devel
 
