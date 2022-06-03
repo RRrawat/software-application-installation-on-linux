e217fe2198ebe5d384e1d0b4114c4b75776ca649
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2022-06-03 11:13:18 +0530

    Update Gradle-installation.sh

--- Gradle-installation.sh
+++ Gradle-installation.sh
@@ -3,5 +3,10 @@
 sudo su
 
 #Gradle requires Java JDK or JRE version 7 or above to be installed.
+#Install the OpenJDK 8 package with the following command:
 sudo yum install java-1.8.0-openjdk-devel
+
+#Verify the Java installation by printing the Java version :
+java -version
+
 
