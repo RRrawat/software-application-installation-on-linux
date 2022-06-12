2f27f2a1237105e29e62224f9993a0d45d10cb47
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2022-06-12 11:13:48 +0530

    Update puppet-installation.sh

--- puppet-installation.sh
+++ puppet-installation.sh
@@ -70,8 +70,23 @@ sudo yum -y install puppetserver
 #Puppet Server is now installed on your master server, but it is not running yet.
 
 #Configure Memory Allocation (optional)
-#By default, Puppet Server is configured to use 2 GB of RAM. You should customize this setting based on how much free memory your master server has, and how many agent nodes it will manage.
+#By default, Puppet Server is configured to use 2 GB of RAM. You should customize this setting based on how much free memory your master server has, 
+#and how many agent nodes it will manage.
 
 #First, open /etc/sysconfig/puppetserver in your favorite text editor. We’ll use vi:
 sudo vi /etc/sysconfig/puppetserver
 
+#Then find the JAVA_ARGS line, and use the -Xms and -Xmx parameters to set the memory allocation. For example, 
+#if you want to use 3 GB of memory, the line should look like this:
+JAVA_ARGS="-Xms3g -Xmx3g"
+
+#Save and exit when you’re done.
+
+#Start Puppet Server
+#Now we’re ready to start Puppet Server with this command:
+sudo systemctl start puppetserver
+
+#Next, enable Puppet Server so that it starts when your master server boots:
+sudo systemctl enable puppetserver
+
+#Puppet Server is running, but it isn’t managing any agent nodes yet. Let’s learn how to install and add Puppet agents!
