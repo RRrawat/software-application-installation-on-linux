b52b4dc6ff31e4b80f7b7198476ea08dcf87879b
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2022-06-24 12:14:32 +0530

    Update kubernates-installation.sh

--- kubernates-installation.sh
+++ kubernates-installation.sh
@@ -1,1 +1,25 @@
+<<Longcommet
+What is a Kubernetes Cluster?
+A Kubernetes cluster consists of a Master and at least one to several worker node(s). The Master is the virtual machine (VM) that administers all activities on your 
+cluster. A node is a VM that serves as a worker machine in your k8s cluster to host running applications. We strongly recommend you only use VMs aka Cloud Servers to 
+run Kubernetes, not system containers aka VPS, as these can cause issues with k8s.
+A node is comprised of the Kubelet, a container runtime, and the kube-proxy. The k8s installation’s three core modules: Kubelet, kubeadm, and kubectl are agents that 
+control the node and communicate with the Kubernetes Master. Once they have been installed and other configurations done, you will be able to create your first k8s 
+cluster. You can manage this cluster from the command line on your kubemaster node.
 
+Every Kubernetes instance runs on top of a container runtime, which is software responsible for managing container operations. Containers in this case are not 
+virtualised servers but rather a solution that packages code and dependencies to run a single application (service) in an isolated (containerised) environment, 
+essentially disassociating applications from the host machine. The most popular and recommended one is Docker, and it’s the one we will use for the purpose of this
+guide. However, if you want to install a different underlying container runtime, you can harness the power of the Container Runtime Interface and use basically any
+runtime you want.
+
+Kubernetes groups containers into pods, its most basic operational unit, which are basically just groups of containers running on the same node. Pods are connected
+over a network and share storage resources.
+
+ 
+
+Prerequisites
+• Multiple CentOS 7 VMs (Cloud Servers) to house the Master and worker nodes.
+• Docker or any other container runtime.
+• User with sudo or root privileges on every server.
+Longcommet
