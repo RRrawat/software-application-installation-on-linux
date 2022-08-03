17bb70e4573ec488c8fc6f616536d1c50f46af9b
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2022-08-03 08:28:03 +0530

    Update kubernates-installation.sh

--- kubernates-installation.sh
+++ kubernates-installation.sh
@@ -323,3 +323,7 @@ kubectl get pods
 #BASH 
 source <(kubectl completion bash) # setup autocomplete in bash into the current shell, bash-completion package should be installed first.
 echo "source <(kubectl completion bash)" >> ~/.bashrc # add autocomplete permanently to your bash shell.
+
+#You can also use a shorthand alias for kubectl that also works with completion:
+alias k=kubectl
+complete -o default -F __start_kubectl k
